bea2e41868cebfd9b6b1edf513f11a54d6dbf60a
thethirdheat <rockstrongo22@gmail.com>
2019-06-13 16:38:09 -0700

    Complete myReduce

--- Javascript/phase_2_enumerable.js
+++ Javascript/phase_2_enumerable.js
@@ -4,7 +4,7 @@ Array.prototype.myEach = function(callback) {
   }
 }
 
-// a = [1,2,3];
+a = [1,2,3];
 // a.myEach((ele) => console.log(ele));
 // a.myEach((ele) => console.log(ele + 3))
 
@@ -14,13 +14,36 @@ function waterbottle(ele) {
 
 a.myEach(waterbottle);
 
+
+
+
 Array.prototype.myMap = function(callback) {
   let ret = []
   this.myEach((ele) => ret.push(callback(ele)));
+  // this.myEach(function(ele) { ret.push(callback(ele))});
   return ret;
 };
 
 console.log(a.myMap((ele) => (ele.toString()+ "waterbottle")));
 console.log(a.map((ele) => (ele.toString() + "waterbottle")));
 
-Array.prototype.myReduce = function(callback)
+Array.prototype.myReduce = function(callback, initialValue = this[0]) {
+  let ret = initialValue
+  this.myEach((ele) => ret = callback(ret, ele));
+  return ret;
+}
+
+function adder(acc, el) {
+  acc += el;
+  return acc;
+}
+
+
+
+function multiplier(acc, el) {
+  acc *= el;
+  return acc;
+}
+
+console.log(a.myReduce(adder, 300));
+console.log(a.myReduce(multiplier));
